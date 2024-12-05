749af6dc2be56a747669b681322eb112dcd35729
zae-hyeong <p0138@naver.com>
2024-12-05 21:54:24 +0900

    [feat] add updateTodoAPI, deleteTodoAPI

--- src/public/utils/todoAPI.ts
+++ src/public/utils/todoAPI.ts
@@ -1,4 +1,4 @@
-import { createTodoRequestBody, TodoCardI } from "../type/todo";
+import { todoRequestBody, TodoCardI } from "../type/todo";
 import { getToken } from "./localStorage";
 
 const API_URL = "http://localhost:8080";
@@ -30,7 +30,7 @@ export async function getTodosAPI() {
   }
 }
 
-export async function createTodoAPI({ title, content }: createTodoRequestBody) {
+export async function createTodoAPI({ title, content }: todoRequestBody) {
   const token = getToken();
 
   if (!token) {
@@ -52,6 +52,62 @@ export async function createTodoAPI({ title, content }: createTodoRequestBody) {
 
   const data = await response.json();
   return data.data as TodoCardI;
+
+  } catch(error) {
+    console.error("There was a problem with the fetch operation:", error);
+    return null;
+  }
+}
+
+export async function updateTodoAPI(todoId: string, { title, content }: todoRequestBody) {
+  const token = getToken();
+
+  if (!token) {
+    console.error("Fail to get token");
+    return null;
+  }
+
+  try {
+  const response = await fetch(API_URL + "/todos/" + todoId, {
+    method: "PUT",
+    body: JSON.stringify({ title, content }),
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: token,
+    },
+  });
+
+  if (!response.ok) throw new Error("api request fail");
+
+  const data = await response.json();
+  return data.data as TodoCardI;
+
+  } catch(error) {
+    console.error("There was a problem with the fetch operation:", error);
+    return null;
+  }
+}
+
+export async function deleteTodoAPI(todoId: string) {
+  const token = getToken();
+
+  if (!token) {
+    console.error("Fail to get token");
+    return null;
+  }
+
+  try {
+  const response = await fetch(API_URL + "/todos/" + todoId, {
+    method: "DELETE",
+    headers: {
+      Authorization: token,
+    },
+  });
+
+  if (!response.ok) throw new Error("api request fail");
+
+  const data = await response.json();
+  return data.data;
 
   } catch(error) {
     console.error("There was a problem with the fetch operation:", error);
